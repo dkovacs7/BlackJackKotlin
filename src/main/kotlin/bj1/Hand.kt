cc23cfe341db73690f7ad5a50155272de338e54b
David Kovacs <dkovacs@callibrity.com>
2018-11-14 15:42:34 -0500

    Simplified Hand class a lot

--- src/main/kotlin/bj1/Hand.kt
+++ src/main/kotlin/bj1/Hand.kt
@@ -4,24 +4,13 @@ class Hand(val name: String) {
 
     private val _cards = mutableListOf<Card>()
 
-    //val cards: List<Card> get() = _cards
-
-    val cards: List<Card>
-        get() {
-            return _cards
-        }
+    val cards: List<Card> get() = _cards
 
     fun add(card: Card) = _cards.add(card)
 
     val size get() = _cards.size
 
-    //val points get() = _cards.sumBy { it.points }
-
-    val points: Int
-        get() {
-            return _cards.sumBy { it.points }
-        }
-
+    val points get() = _cards.sumBy { it.points }
 
 }
 
