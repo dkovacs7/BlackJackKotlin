a6e2350d303bb3e2b9085996d1c4335e0441ebfd
David Kovacs <dkovacs@callibrity.com>
2018-11-14 14:10:18 -0500

    Changed Hand into something that looks more like old school java

--- src/main/kotlin/bj1/Hand.kt
+++ src/main/kotlin/bj1/Hand.kt
@@ -4,13 +4,53 @@ class Hand(val name: String) {
 
     private val _cards = mutableListOf<Card>()
 
-    val cards: List<Card> get() = _cards
+    //val cards: List<Card> get() = _cards
 
-    fun add(card: Card) = _cards.add(card)
+    val cards: List<Card>
+        get() {
+            return _cards
+        }
 
-    val size get() = _cards.size
 
-    val points get() = _cards.sumBy { it.points }
+    //        val a = fun() {
+    //            println(111)
+    //        }
+    //
+    //        val b = fun(p1: Int) = p1 * p1
+
+
+    // fun add(card: Card) = _cards.add(card)
+
+    fun add(card: Card) {
+        _cards.add(card)
+    }
+
+
+    //val size get() = _cards.size
+
+    val size: Int
+        get() {
+            return _cards.size
+        }
+
+
+    //val points get() = _cards.sumBy { it.points }
+
+//    val points: Int
+//        get() {
+//            return _cards.sumBy { it.points }
+//        }
+
+
+    val points: Int
+        get() {
+            var retVal = 0
+            _cards.forEach {
+                retVal += it.points }
+            return retVal
+        }
+
+
 
 }
 
